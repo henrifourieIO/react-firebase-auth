e1d6da6b93c6d739277fa34501e514b6f56090af
HenriFourie <61292359+henrifourieIO@users.noreply.github.com>
2022-01-18 20:26:22 +0200

    Finilize

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -1,30 +1,57 @@
 import React, { useRef, useState } from "react";
 import { Card, Form, Button, Modal, Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  reauthenticateWithCredential,
+  EmailAuthProvider,
+  updateEmail,
+} from "firebase/auth";
 
 const UpdateProfile = () => {
   const emailRef = useRef();
+  const credEmailRef = useRef();
+  const credPasswordRef = useRef();
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
   const [show, setShow] = useState("");
   const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState();
 
   const handleSubmit = async () => {
     setLoading(true);
-    let auth = getAuth();
     setSuccess("Email Changed!");
     setShow(!show);
+    setEmail(emailRef.current.value);
+  };
+
+  const handleCredSubmit = async () => {
+    let auth = getAuth();
+    let user = auth.currentUser;
+
+    try {
+      const credential = EmailAuthProvider.credential(
+        user.email,
+        credPasswordRef.current.value
+      );
+      await reauthenticateWithCredential(user, credential).then((res) => {
+        updateEmail(res.user, email).then((res) => {
+          console.log("updateEmail");
+          console.log(res);
+        }).catch((error) => {
+          console.log("updateEmailError");
+          console.log(error.message);
+        })
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+    setShow(false);
     setLoading(false);
   };
 
   return (
     <>
-      <Modal show={show} centerd onHide={() => setShow(!show)}>
-        <Modal.Header closeButton>
-          <h2>Re-Authenticate</h2>
-        </Modal.Header>
-      </Modal>
       <Card className="p-4">
         <h2 className="text-center mb-3">Update Profile</h2>
         {!success ? (
@@ -42,7 +69,7 @@ const UpdateProfile = () => {
             }}
           >
             <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
+              <Form.Label>New Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
             <Button disabled={loading} className="w-100 mt-4" type="submit">
@@ -58,6 +85,33 @@ const UpdateProfile = () => {
           </Link>
         )}
       </Card>
+
+      {/* Re-Login Modal */}
+      <Modal show={show} centered onHide={() => setShow(!show)}>
+        <Modal.Header closeButton>
+          <h2 className={"text-center"}>Re-Authenticate</h2>
+        </Modal.Header>
+        <Modal.Body>
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleCredSubmit();
+            }}
+          >
+            <Form.Group id="credEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={credEmailRef} required />
+            </Form.Group>
+            <Form.Group id="credPassword">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="password" ref={credPasswordRef} required />
+            </Form.Group>
+            <Button className="w-100 mt-4" type="submit">
+              Login
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </>
   );
 };
